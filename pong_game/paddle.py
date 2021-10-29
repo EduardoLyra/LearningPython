adfcad95cf20af508bd9e33e4c5aa3e417ca0fb0
EduardoLyra <eduardolyra14@gmail.com>
2021-10-29 15:47:35 -0300

    Pong project

---
+++ pong_game/paddle.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+
+class Paddle(Turtle):
+    def __init__(self, position):
+        super().__init__()
+        self.create_paddle(position)
+
+    def create_paddle(self, position):
+        self.color('white')
+        self.shape('square')
+        self.penup()
+        self.shapesize(stretch_len=1, stretch_wid=5)
+        self.goto(position)
+
+    def up(self):
+        new_y = self.ycor() + 20
+        self.goto(self.xcor(), new_y)
+
+    def down(self):
+        new_y = self.ycor() - 20
+        self.goto(self.xcor(), new_y)
--- snake_game/main.py
+++ snake_game/main.py
@@ -9,7 +9,7 @@ def main():
     screen = Screen()
     screen.setup(width=600, height=600)
     screen.bgcolor('black')
-    screen.title('My Snake Game')
+    screen.title('Snake Game')
     screen.tracer(0)
 
     snake = Snake()
