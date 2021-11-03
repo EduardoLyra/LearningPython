da57adc9f012e2c5304576be395da6e697e5f89c
EduardoLyra <eduardolyra14@gmail.com>
2021-11-03 15:32:22 -0300

    turtle crossing

--- turtle-crossing/main.py
+++ turtle-crossing/main.py
@@ -1,4 +1,4 @@
-import time
+from time import sleep
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
@@ -9,7 +9,9 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
-car = CarManager()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
 
 screen.listen()
 
@@ -18,8 +20,18 @@ screen.onkeypress(player.move, 'Up')
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
-    car.create_car()
+    sleep(0.1)
     screen.update()
+    car_manager.create_car()
+    car_manager.move_cars()
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            scoreboard.game_over()
+            game_is_on = False
+    if player.ycor() > 280:
+        scoreboard.add_score()
+        car_manager.level_up()
+        player.restart()
+
 
 screen.exitonclick()
