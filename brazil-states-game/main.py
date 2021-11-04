22c188957f2d1ecb4c806899ddce731e6046975e
BlackSkull8845 <50972059+BlackSkull8845@users.noreply.github.com>
2021-11-03 22:03:10 -0300

    bazil states game

---
+++ brazil-states-game/main.py
@@ -0,0 +1,16 @@
+import turtle
+
+screen = turtle.Screen()
+screen.title("Brazil States Game")
+image = "brazil-states-game/blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+
+def get_mouse_click_coor(x, y):
+    print(x, y)
+
+
+turtle.onscreenclick(get_mouse_click_coor)
+
+turtle.mainloop()
